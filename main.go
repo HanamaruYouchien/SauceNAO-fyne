ff9d2a46ec9d492b37710298f9da40d1a4e3482f
LTSlw <38352100+LTSlw@users.noreply.github.com>
2024-05-17 14:49:35 +0800

    Add component ImagePicker

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"strconv"
 
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/dialog"
 
 	"github.com/HanamaruYouchien/SauceNAO-fyne/ui/component"
 )
@@ -28,5 +29,24 @@ func main() {
 
 	w3 := a.NewWindow("Image Url")
 	w3.SetContent(component.ImageUrlEntry())
-	w3.ShowAndRun()
+
+	w4 := a.NewWindow("ImagePicker")
+	imageMethod := component.METHOD_FILE
+	w4.SetContent(component.ImagePicker(&w4, func(method bool) {
+		imageMethod = method
+	}, func(img *[]byte) {
+		image = img
+		fmt.Println(strconv.Itoa(len(*image)))
+	}, func() {
+		msg := "Method: "
+		switch imageMethod {
+		case component.METHOD_FILE:
+			msg += "File\nFile size:" + strconv.Itoa(len(*image))
+		case component.METHOD_URL:
+			msg += "URL"
+		}
+		dialog.NewInformation("Query", msg, w4).Show()
+	}))
+	w4.ShowAndRun()
+	_ = imageMethod
 }
--- ui/component/imagePicker.go
+++ ui/component/imagePicker.go
@@ -4,10 +4,49 @@ import (
 	"io"
 
 	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/dialog"
+	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/storage"
 	"fyne.io/fyne/v2/widget"
 )
+
+const (
+	METHOD_FILE = false
+	METHOD_URL  = true
+)
+
+func ImagePicker(win *fyne.Window, setMethod func(bool), onSelectImage func(image *[]byte), onSearch func()) *fyne.Container {
+	const (
+		OPTION_FILE = "File"
+		OPTION_URL  = "URL"
+	)
+	title := widget.NewLabel("Search by")
+
+	selector := ImageFileSelector(win, onSelectImage)
+
+	entry := ImageUrlEntry()
+
+	radio := widget.NewRadioGroup([]string{OPTION_FILE, OPTION_URL}, func(val string) {
+		state := true
+		switch val {
+		case OPTION_FILE:
+			state = METHOD_FILE
+		case OPTION_URL:
+			state = METHOD_URL
+		}
+		setMethod(state)
+		selector.Hidden = state
+		entry.Hidden = !state
+	})
+	radio.Horizontal = true
+	radio.SetSelected(OPTION_FILE)
+	switchMethodContainer := container.New(layout.NewHBoxLayout(), title, layout.NewSpacer(), radio)
+
+	btnSearch := widget.NewButton("Search", onSearch)
+
+	return container.New(layout.NewVBoxLayout(), switchMethodContainer, selector, entry, btnSearch)
+}
 
 func ImageUrlEntry() *widget.Entry {
 	entry := widget.NewEntry()
